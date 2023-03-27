ceeec4cef76860b41a42985069e086335a6885d9
Julia Isaksson <112895681+Juliastarstrike@users.noreply.github.com>
2023-03-27 16:03:21 +0200

    process of player movement

--- Cafe/Assets/Scripts/Julia/PlayerMovement.cs
+++ Cafe/Assets/Scripts/Julia/PlayerMovement.cs
@@ -31,28 +31,32 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        MovePlayer();
+        
         //ground check
         grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
         
         MyInput();
+        speedControl();
 
         // handel drag
         if (grounded)
             rb.drag = groundDrag;
+           
         else
             rb.drag = 0;
+            
     }
 
     private void FixedUpdate() 
     {
-        
+        MovePlayer();
     }
 
     private void MyInput()
     {
-        horizontalInput = Input.GetAxisRaw("Horzontal");
+        horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
+        Debug.Log("000000000000");
     }
 
     private void MovePlayer()
@@ -61,5 +65,17 @@ public class PlayerMovement : MonoBehaviour
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
         rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+    }
+
+    private void speedControl()
+    {
+        Vector2 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        // limit velocity if needed
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized *  moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
     }
 }
