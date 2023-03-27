1fbb31a312d7b75d6f2019f283feb329d17338a2
Julia Isaksson <112895681+Juliastarstrike@users.noreply.github.com>
2023-03-27 15:25:13 +0200

    Player movement in process
    camera can rotate but player can not move yet

--- Cafe/Assets/Scripts/Julia/PlayerMovement.cs
+++ Cafe/Assets/Scripts/Julia/PlayerMovement.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //Movement
+    public float moveSpeed;
+    public float groundDrag;
+
+    //Ground check
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
+
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+        //ground check
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
         
+        MyInput();
+
+        // handel drag
+        if (grounded)
+            rb.drag = groundDrag;
+        else
+            rb.drag = 0;
+    }
+
+    private void FixedUpdate() 
+    {
+        
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horzontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void MovePlayer()
+    {
+        //calculate movement direction
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
     }
 }
