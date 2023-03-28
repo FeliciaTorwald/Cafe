f2a19772922877a3bd4ee9f8186ffa7147686c14
Julia Isaksson <112895681+Juliastarstrike@users.noreply.github.com>
2023-03-28 10:25:20 +0200

    Player movement done

--- Cafe/Assets/Scripts/Julia/PlayerMovement.cs
+++ Cafe/Assets/Scripts/Julia/PlayerMovement.cs
@@ -5,15 +5,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     //Movement
-    public float moveSpeed;
+    public float moveSpeed = 15;
     public float groundDrag;
-
     public float jumpForce;
     public float jumpCooldown;
     public float airMultiplier;
     bool readyToJump;
-
     public KeyCode jumpKey = KeyCode.Space;
+
     //Ground check
     public float playerHeight;
     public LayerMask whatIsGround;
@@ -21,36 +20,24 @@ public class PlayerMovement : MonoBehaviour
 
 
     public Transform orientation;
-
     float horizontalInput;
     float verticalInput;
     Vector3 moveDirection;
-
     Rigidbody rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+
+        //set how much the player movement can slide on the ground
+        rb.drag = 7;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
-        //ground check
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
-        
         MyInput();
-        speedControl();
-
-        // handel drag
-        if (grounded)
-            rb.drag = groundDrag;
-           
-        else
-            rb.drag = 0;
-            
+        speedControl();       
     }
 
     private void FixedUpdate() 
@@ -62,15 +49,6 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
-        
-        //when jump
-        if(Input.GetKey(jumpKey) && readyToJump && grounded)
-        {
-            readyToJump = false;
-
-            Jump();
-            Invoke(nameof(ResetJump), jumpCooldown);
-        }
     }
 
     private void MovePlayer()
@@ -97,16 +75,5 @@ public class PlayerMovement : MonoBehaviour
             Vector3 limitedVel = flatVel.normalized *  moveSpeed;
             rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
         }
-    }
-
-    private void Jump()
-    {
-        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-
-        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
-    }
-    private void ResetJump()
-    {
-        readyToJump = true;
     }
 }
