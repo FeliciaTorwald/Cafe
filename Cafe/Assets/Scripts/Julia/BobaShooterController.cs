9978f078b93cede7e28515f2456a746803da0771
Julia Isaksson <112895681+Juliastarstrike@users.noreply.github.com>
2023-04-04 10:53:11 +0200

    Ball shooterr works with one ball that is in the scene
    works means, you can pick it up, hold done space to aim and when you relese the ball get thrown to an target and you need to pick it up again to throw it again.

--- Cafe/Assets/Scripts/Julia/BobaShooterController.cs
+++ Cafe/Assets/Scripts/Julia/BobaShooterController.cs
@@ -11,11 +11,11 @@ public class BobaShooterController : MonoBehaviour
     public Transform PosOverHead;
     public Transform posDribble;
     public Transform Target;
-    private float T = 0;
 
     //variables
-    private bool InBallHands = true;
+    public bool IsBallInHands = false;
     private bool IsBallFlying = false;
+    private float T = 0;
 
     void Start()
     {
@@ -25,39 +25,36 @@ public class BobaShooterController : MonoBehaviour
     void Update()
     {
         //walkin
-        Debug.Log("1");
         Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        Debug.Log("2");
         transform.position += direction * MoveSpeed * Time.deltaTime;
-        Debug.Log("3");
         transform.LookAt(transform.position + direction);
         
         //Ball in hands
-        if (InBallHands)
+        if (IsBallInHands == true)
         {
-        
-        //Hold over head
-        if (Input.GetKey(KeyCode.Space))
-        {
-            Ball.position = PosOverHead.position;
-            Arms.localEulerAngles = Vector3.right * 180;
+            //Hold over head
+            if (Input.GetKey(KeyCode.Space))
+            {
+               Ball.position = PosOverHead.position;
+               //Arms.localEulerAngles = Vector3.right * 180;
 
-            transform.LookAt(Target.position);
+               transform.LookAt(Target.position);
+            }
+            //dribbling
+            else
+            {
+                Ball.position = posDribble.position;
+                //Arms.localEulerAngles = Vector3.right * 1;
+            } 
+            //throw ball
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                IsBallInHands = false;
+                IsBallFlying = true;
+                T = 0;
+            }
         }
-        //dribbling
-        else
-        {
-            Ball.position = posDribble.position + Vector3.up * Mathf.Abs(Mathf.Sin(Time.time * 5));
-            Arms.localEulerAngles = Vector3.right * 0;
-        }
-        }
-        //throw ball
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            InBallHands = false;
-            IsBallFlying = true;
-            T = 0;
-        }
+
 
         //ball in the air
          if (IsBallFlying)
@@ -73,7 +70,6 @@ public class BobaShooterController : MonoBehaviour
 
             // move in arc
             Vector3 arc = Vector3.up * 5 * Mathf.Sin(t01 * 3.14f);
-
             Ball.position = pos + arc;
 
             if (t01 >= 1)
@@ -82,5 +78,17 @@ public class BobaShooterController : MonoBehaviour
                 Ball.GetComponent<Rigidbody>().isKinematic = false;
             }
          }
+    }
+
+    private void OnTriggerStay(Collider other) 
+    {
+        if (Input.GetKey(KeyCode.E))
+            {
+            if(other.gameObject.tag == "BobaPearls" && !IsBallInHands && !IsBallFlying)
+                {
+                    IsBallInHands = true;
+                    Ball.GetComponent<Rigidbody>().isKinematic = true;
+                }
+            }
     }
 }
