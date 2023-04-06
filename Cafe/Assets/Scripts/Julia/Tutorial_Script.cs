c38e9540ef01080445d11d610673ac1796cfbab5
Julia Isaksson <112895681+Juliastarstrike@users.noreply.github.com>
2023-04-06 11:09:15 +0200

    Start of UI done

--- Cafe/Assets/Scripts/Julia/Tutorial_Script.cs
+++ Cafe/Assets/Scripts/Julia/Tutorial_Script.cs
@@ -5,38 +5,19 @@ using UnityEngine;
 public class Tutorial_Script : MonoBehaviour
 {
     public GameObject ShowControllsDone;
-    bool Escapepushed = false;
+    bool DoNotShowControllsbool = true;
 
     void Update()
-    {
-        if (Escapepushed == false)
-        {
-           if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            Invoke("DoNotShowControlls", 0);
-     
-        }
-        
-        }
-
-        if (Escapepushed == true)
-        {
-           if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            Invoke("DoShowControlls", 0);
-           
-        } 
-        }
-
-        //When player tested all controlls stop showing controlls after ... seconds
-    }
-        private void DoNotShowControlls()
-        {
-            ShowControllsDone.SetActive(false);
-            
-        }
-         private void DoShowControlls()
+    {   
+       if (Input.GetKeyDown(KeyCode.Escape) && DoNotShowControllsbool == false)
         {
             ShowControllsDone.SetActive(true);
+            DoNotShowControllsbool = true;
         }
+        else if (Input.GetKeyDown(KeyCode.Escape) && DoNotShowControllsbool == true)
+        {
+            ShowControllsDone.SetActive(false);
+            DoNotShowControllsbool = false;
+        }
+    }
 }
