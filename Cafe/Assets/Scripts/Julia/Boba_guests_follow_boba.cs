65834e8fed899dc3c699f186e65fd2957e1cd799
Julia Isaksson <112895681+Juliastarstrike@users.noreply.github.com>
2023-04-12 16:11:24 +0200

    Main scene update

--- Cafe/Assets/Scripts/Julia/Boba_guests_follow_boba.cs
+++ Cafe/Assets/Scripts/Julia/Boba_guests_follow_boba.cs
@@ -8,27 +8,31 @@ public class Boba_guests_follow_boba : MonoBehaviour
     public GameObject[] allBobaPearls;
     public GameObject closestBoba;
 
+    bool ful_Mouth = false;
+    float speed = 1.5f;
+    float Boba_In_Mouth = 0;
+
     void Start()
     {
         allBobaPearls = GameObject.FindGameObjectsWithTag(tagToDetect);
+        Boba_In_Mouth = 0;
     }
 
     private void Update()
     {
-        //Vector3 BobaPos = boba.transform.position;
-        //transform.position = Vector3.MoveTowards(transform.position, BobaPos, speed * Time.deltaTime);
         closestBoba = FindClosestBoba();
-        print(closestBoba.name);
+        //print(closestBoba.name);  
+        transform.position = Vector3.MoveTowards(transform.position, closestBoba.transform.position, speed * Time.deltaTime);
     }
 
     public GameObject FindClosestBoba()
     {
-        GameObject[] Bobas;
-        Bobas = GameObject.FindGameObjectsWithTag("BobaPearls");
+        GameObject[] Boba_pearls;
+        Boba_pearls = GameObject.FindGameObjectsWithTag("BobaPearls");
         GameObject closest = null;
         float distance = Mathf.Infinity;
         Vector3 position = transform.position;
-        foreach (GameObject Boba in Bobas)
+        foreach (GameObject Boba in Boba_pearls)
         {
             Vector3 diff = Boba.transform.position - position;
             float curDistance = diff.sqrMagnitude;
@@ -39,5 +43,21 @@ public class Boba_guests_follow_boba : MonoBehaviour
             }
         }
         return closest;
+    }
+
+    private void Ontrigger(Collider other) 
+    {
+        Debug.Log("collision");
+        if (other.tag == "BobaPearls" && ful_Mouth == false)
+        {
+            Debug.Log("hello");
+            Boba_In_Mouth += 1;
+            Destroy(closestBoba);
+            
+            if (Boba_In_Mouth >= 5)
+            {
+                ful_Mouth = true;
+            }
+        }
     }
 }
