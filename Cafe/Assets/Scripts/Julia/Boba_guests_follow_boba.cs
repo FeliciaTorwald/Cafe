5877c0ce296f3d5246d399f429ca9a74a8fecfc4
Julia Isaksson <112895681+Juliastarstrike@users.noreply.github.com>
2023-04-19 13:59:14 +0200

    player can go to eating boba guest and hit it with space but it only prints in console

--- Cafe/Assets/Scripts/Julia/Boba_guests_follow_boba.cs
+++ Cafe/Assets/Scripts/Julia/Boba_guests_follow_boba.cs
@@ -7,9 +7,10 @@ public class Boba_guests_follow_boba : MonoBehaviour
     public string tagToDetect = "BobaPearls";
     public GameObject[] allBobaPearls;
     public GameObject closestBoba;
+    public Transform toTheExit;
 
     bool ful_Mouth = false;
-    float speed = 1.5f;
+    float speed = 2f;
     float Boba_In_Mouth = 0;
 
     void Start()
@@ -22,7 +23,15 @@ public class Boba_guests_follow_boba : MonoBehaviour
     {
         closestBoba = FindClosestBoba();
         //print(closestBoba.name);  
+        if(ful_Mouth == false)
+        {
         transform.position = Vector3.MoveTowards(transform.position, closestBoba.transform.position, speed * Time.deltaTime);
+        }
+
+        if(ful_Mouth == true)
+        {
+        transform.position = Vector3.MoveTowards(transform.position, toTheExit.position, speed * Time.deltaTime);
+        }
     }
 
     public GameObject FindClosestBoba()
