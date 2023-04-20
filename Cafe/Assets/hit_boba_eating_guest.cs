b8b4cd0292c7baa304d138643a9ca8d34189a58d
FeliciaTorwald <113012104+FeliciaTorwald@users.noreply.github.com>
2023-04-20 14:14:38 +0200

    can only hit guest with mop not your hands

--- Cafe/Assets/hit_boba_eating_guest.cs
+++ Cafe/Assets/hit_boba_eating_guest.cs
@@ -6,6 +6,7 @@ public class hit_boba_eating_guest : MonoBehaviour
 {
     bool intriggerarea = false;
     public bool Boba_guests_got_hit = false;
+    bool holdMop;
 
     void Start()
     { 
@@ -15,9 +16,10 @@ public class hit_boba_eating_guest : MonoBehaviour
     void Update()
     //If you are close to boba eating quest and press space print "slå boba gäst"
     {
+       
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                if (intriggerarea == true)
+                if (intriggerarea == true && holdMop)
                 {
                     Debug.Log("slå boba ätar gästen!");
                     Boba_guests_got_hit = true;
@@ -30,12 +32,22 @@ public class hit_boba_eating_guest : MonoBehaviour
         {
             intriggerarea = true;
         }
+
+        if(other.gameObject.tag == "Mop")
+        {
+            holdMop = true;
+        }
     }
     private void OnTriggerExit(Collider other) 
     {
         if (other.gameObject.tag == "boba eating guests")
         {
             intriggerarea = false;
+        }
+
+        if (other.gameObject.tag == "Mop")
+        {
+            holdMop = false;
         }
     }
 }
