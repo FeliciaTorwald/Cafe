b16ae8c1a9e3576e233e241d6519eed77f646386
Julia Isaksson <112895681+Juliastarstrike@users.noreply.github.com>
2023-04-21 10:50:42 +0200

    Eating boba guest 2.0
    my computer have some unnone problems so we are going to test push some things up and do the rest on felicias computer.

--- Cafe/Assets/Scripts/Julia/Boba_guests_follow_boba.cs
+++ Cafe/Assets/Scripts/Julia/Boba_guests_follow_boba.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Boba_guests_follow_boba : MonoBehaviour
 {
@@ -11,51 +12,64 @@ public class Boba_guests_follow_boba : MonoBehaviour
     public Transform toTheExit;
     public Transform spat_out_boba;
     hit_boba_eating_guest boba_eating_guest_got_hit_true;
+    private NavMeshAgent nav;
 
+    
     bool ful_Mouth = false;
-    float speed = 2f;
+    float speed = 500f;
     public float Boba_In_Mouth = 0;
 
     void Start()
     {
         boba_eating_guest_got_hit_true = FindFirstObjectByType<hit_boba_eating_guest>();
 
+        //Find all bobaPearls
         allBobaPearls = GameObject.FindGameObjectsWithTag(tagToDetect);
+
         Boba_In_Mouth = 0;
+        nav = GetComponent<NavMeshAgent>();
+
+        //Invoke("closestBobas", 3.0f);
+        InvokeRepeating(nameof(closestBobas), 1,1);
     }
 
     private void Update()
     {
+        //Debug.Log(Boba_In_Mouth);
 
         //Ha inte denna i update utan kolla först på vilken är närmsta boba och sen när bobans förstörs kolla var nästa ligger
-        closestBoba = FindClosestBoba();
+        
+        
+        //nav.destination = FindClosestBoba();
 
         //print(closestBoba.name);  
-        if (ful_Mouth == false && boba_eating_guest_got_hit_true.Boba_guests_got_hit == false)
+        if(ful_Mouth == false && boba_eating_guest_got_hit_true.Boba_guests_got_hit == false)
         {
-            transform.position = Vector3.MoveTowards(transform.position, closestBoba.transform.position, speed * Time.deltaTime);
+        //nav.destination = Vector3.MoveTowards(transform.position, closestBoba.transform.position, speed * Time.deltaTime);
+        nav.destination = closestBoba.transform.position;
         }
 
-
-        if (ful_Mouth == true)
+        if(ful_Mouth == true)
         {
-            transform.position = Vector3.MoveTowards(transform.position, toTheExit.position, speed * Time.deltaTime);
+        //nav.destination = Vector3.MoveTowards(transform.position, toTheExit.position, speed * Time.deltaTime);
+        nav.destination = toTheExit.position;
         }
 
-        if (boba_eating_guest_got_hit_true.Boba_guests_got_hit == true)
+        if(boba_eating_guest_got_hit_true.Boba_guests_got_hit == true)
         {
-            for (int i = 0; i < Boba_In_Mouth; i++)
-            {
-                Debug.Log("Spawn boba");
-                Instantiate(preFab, spat_out_boba.position, Quaternion.identity);
-                boba_eating_guest_got_hit_true.Boba_guests_got_hit = false;
-            }
-            ful_Mouth = true;
+                for (int i = 0; i < Boba_In_Mouth; i++) 
+                {
+                    Debug.Log("Spawn boba");
+                    Instantiate(preFab, spat_out_boba.position, Quaternion.identity);
+                    boba_eating_guest_got_hit_true.Boba_guests_got_hit = false;
+                }
+                ful_Mouth = true;
         }
     }
 
     public GameObject FindClosestBoba()
     {
+        //List over boba perls
         GameObject[] Boba_pearls;
         Boba_pearls = GameObject.FindGameObjectsWithTag("BobaPearls");
         GameObject closest = null;
@@ -74,17 +88,26 @@ public class Boba_guests_follow_boba : MonoBehaviour
         return closest;
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other) 
     {
         if (other.gameObject.tag == "BobaPearls" && ful_Mouth == false)
         {
-            Boba_In_Mouth += 1;
+            Boba_In_Mouth +=1;
+            Debug.Log(Boba_In_Mouth);
+            //Debug.Log(Boba_In_Mouth);
             closestBoba.SetActive(false);
-
+            //closestBoba = FindClosestBoba();
+            
             if (Boba_In_Mouth >= 5)
             {
                 ful_Mouth = true;
             }
         }
+    }
+
+    public void closestBobas()
+    {
+    //find first boba
+    closestBoba = FindClosestBoba();
     }
 }
