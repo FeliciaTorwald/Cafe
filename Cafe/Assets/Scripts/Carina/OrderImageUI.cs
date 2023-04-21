c4dac1b290f95617e67ca5568175fa5517eaeed3
Cym3ra <112971484+Cym3ra@users.noreply.github.com>
2023-04-21 09:31:34 +0200

    Fixes

--- Cafe/Assets/Scripts/Carina/OrderImageUI.cs
+++ Cafe/Assets/Scripts/Carina/OrderImageUI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class OrderPopUpUI : MonoBehaviour
+public class OrderImageUI : MonoBehaviour
 {
 
     [SerializeField] GameObject orderImage;
