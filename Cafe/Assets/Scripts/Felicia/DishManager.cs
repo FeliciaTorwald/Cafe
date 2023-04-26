b96b9b4e3bad0133ecc70c53b5728eba5eb4a297
FeliciaTorwald <113012104+FeliciaTorwald@users.noreply.github.com>
2023-04-26 15:48:47 +0200

    Commented out code that threw clean dish out of the pond

--- Cafe/Assets/Scripts/Felicia/DishManager.cs
+++ Cafe/Assets/Scripts/Felicia/DishManager.cs
@@ -13,22 +13,22 @@ public class DishManager : MonoBehaviour
         bTH = FindObjectOfType<BobaTeaHandler>();
     }
 
-    void Spawn()
-    {
-        GameObject cleanDish = Instantiate(preFab, spawnPos.transform.position, Quaternion.identity);
+    //void Spawn()
+    //{
+    //    GameObject cleanDish = Instantiate(preFab, spawnPos.transform.position, Quaternion.identity);
 
-        cleanDish.GetComponent<Rigidbody>().isKinematic = false;
-        cleanDish.transform.position += Random.insideUnitSphere + new Vector3(0, 1.2f, 0);
-        Vector3 force = Random.insideUnitSphere * 5;
-        force.y = Mathf.Abs(force.y);
-        cleanDish.GetComponent<Rigidbody>().AddForce(force, ForceMode.Impulse);
-    }
+    //    cleanDish.GetComponent<Rigidbody>().isKinematic = false;
+    //    cleanDish.transform.position += Random.insideUnitSphere + new Vector3(0, 1.2f, 0);
+    //    Vector3 force = Random.insideUnitSphere * 5;
+    //    force.y = Mathf.Abs(force.y);
+    //    cleanDish.GetComponent<Rigidbody>().AddForce(force, ForceMode.Impulse);
+    //}
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "DirtyBobatea")
         {
             bTH.DestroyDish();
-            Spawn();
+            //Spawn();
         }
     }
 }
