30c592977f9ba7ac07225bf58a9a3712548474ba
FeliciaTorwald <113012104+FeliciaTorwald@users.noreply.github.com>
2023-05-02 16:20:51 +0200

    Update Interactable_NewBoba.cs

--- Cafe/Assets/Scripts/Carl/NewInteractionSystem/Interactable_NewBoba.cs
+++ Cafe/Assets/Scripts/Carl/NewInteractionSystem/Interactable_NewBoba.cs
@@ -5,6 +5,17 @@ using UnityEngine;
 
 public class Interactable_NewBoba : NewAbstractInteractable
 {
+    public Transform Target;
+    Vector3 startThrowPos;
+    private float t = 0;
+    private void Start()
+    {
+        Target = GameObject.Find("TargetPoint Boba shooter").transform;
+    }
+    private void Update()
+    {
+        t += Time.deltaTime;
+    }
     public override void Interact(NewInteract newInteract)
     {
         playerInteractRef = newInteract;
@@ -22,7 +33,23 @@ public class Interactable_NewBoba : NewAbstractInteractable
     public override void Throw(NewInteract newInteract)
     {
         //Implement throwing functionality
-        
+        float duration = 1.5f;
+        float t01 = t / duration;
+
+        // move to target
+        Vector3 A = startThrowPos;
+        Vector3 B = Target.position;
+        Vector3 pos = Vector3.Lerp(A, B, t01);
+
+        // move in arc
+        Vector3 arc = Vector3.up * 5 * Mathf.Sin(t01 * 3.14f);
+        gameObject.transform.position = pos + arc;
+
+
+        if (t01 >= 2)
+        {
+            gameObject.GetComponent<Rigidbody>().isKinematic = false;
+        }
         //Ensure the below function call is included
         playerInteractRef.NoLongerHoldingSomething();
     }
