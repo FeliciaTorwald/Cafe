90efaea3ad72b13a8f58998962a36640a0589570
Cym3ra <112971484+Cym3ra@users.noreply.github.com>
2023-05-08 22:12:37 +0200

    Added crafting tea sort 2

--- Cafe/Assets/Scripts/Carina/Crafting/CraftingUI.cs
+++ Cafe/Assets/Scripts/Carina/Crafting/CraftingUI.cs
@@ -7,6 +7,70 @@ using TMPro;
 public class CraftingUI : MonoBehaviour
 {
 
+    public CraftingRecipe recipe1, recipe2;
+
+    private bool canCraft1, canCraft2;
+
+    public void UpdateCanCraft()
+    {
+        canCraft1 = true;
+        canCraft2 = true;
+
+        for (int i = 0; i < recipe1.cost.Length; i++)
+        {
+            if (!Inventory.instance.HasItems(recipe1.cost[i].item, recipe1.cost[i].quantity))
+            {
+                canCraft1 = false;
+                break;
+            }
+
+        }
+        for (int i = 0; i < recipe2.cost.Length; i++)
+        {
+            if (!Inventory.instance.HasItems(recipe2.cost[i].item, recipe2.cost[i].quantity))
+            {
+                canCraft2 = false;
+                break;
+            }
+        }
+
+        FindObjectOfType<Resource>().CheckKettleForWater();
+        CheckCanMakeTea();
+        CheckCanMakeOtherTea();
+    }
+
+    public void OnClickButton()
+    {
+        if (canCraft1)
+        {
+            CraftingTea.instance.Craft(recipe1);
+        }
+        else if (canCraft2)
+        {
+            CraftingTea.instance.Craft(recipe2);
+        }
+    }
+
+    public void CheckCanMakeTea()
+    {
+        if (canCraft1)
+        {
+            CraftingTea.instance.Craft(recipe1);
+        }
+
+    }
+
+    public void CheckCanMakeOtherTea()
+    {
+        if (canCraft2)
+        {
+            CraftingTea.instance.Craft(recipe2);
+        }
+    }
+
+
+
+    /*
     public CraftingRecipe recipe;
 
 
@@ -44,5 +108,5 @@ public class CraftingUI : MonoBehaviour
         {
             CraftingTea.instance.Craft(recipe);
         }
-    }
+    }*/
 }
