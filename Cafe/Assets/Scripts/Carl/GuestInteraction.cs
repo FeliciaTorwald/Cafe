5c69a958b7b8a5b9b50795996ca73a26a65059cb
Carl Zeidler <carl.zeidler@gmail.com>
2023-05-09 09:54:21 +0200

    Anger meter changes color as it progresses

--- Cafe/Assets/Scripts/Carl/GuestInteraction.cs
+++ Cafe/Assets/Scripts/Carl/GuestInteraction.cs
@@ -10,11 +10,16 @@ public class GuestInteraction : MonoBehaviour
     public BobaTeaHandler table;
     public float irritation;
     public float maxIrritationBeforeLeaving;
+    [SerializeField] private RectTransform guestCanvasRef;
     [SerializeField] private Slider angerMeter;
-
+    [SerializeField] private Image sliderFill;
+    [SerializeField] private Color minIrritationColor;
+    [SerializeField] private Color maxIrritationColor;
+    
     private void Start()
     {
         angerMeter.maxValue = maxIrritationBeforeLeaving;
+        guestCanvasRef.GetComponent<Canvas>().worldCamera = Camera.main;
     }
 
     private void Update()
@@ -48,6 +53,7 @@ public class GuestInteraction : MonoBehaviour
         }
     }
 
+
     public void ServeGuest(TeaType teaType)
     {
         //Add check for whether the correct tea is served or not
@@ -69,5 +75,43 @@ public class GuestInteraction : MonoBehaviour
     private void UpdateAngerMeter()
     {
         angerMeter.value = irritation;
+        ChangeAngerMeterColor();
+        // CheckOffScreenPosition();
+    }
+
+
+    private void ChangeAngerMeterColor()
+    {
+        float fillAmount = angerMeter.value / angerMeter.maxValue;
+        sliderFill.color = Color.Lerp(minIrritationColor, maxIrritationColor, fillAmount);
+    }
+    
+    private void CheckOffScreenPosition()
+    {
+        Transform parentObject = parentGuest.gameObject.transform;
+        Vector3 localPos = parentObject.InverseTransformPoint(guestCanvasRef.position);
+        Vector3 screenPos = Camera.main.WorldToViewportPoint(parentObject.TransformPoint(localPos));
+        Vector2 anchorPos = guestCanvasRef.anchorMax;
+
+        if (screenPos.x < 0f)
+        {
+            anchorPos.x = 0f;
+        }
+        else if (screenPos.x > 1f)
+        {
+            anchorPos.x = 1f;
+        }
+
+        if (screenPos.y < 0f)
+        {
+            anchorPos.y = 0f;
+        }
+        else if (screenPos.y > 1f)
+        {
+            anchorPos.y = 1f;
+        }
+
+        guestCanvasRef.anchorMax = anchorPos;
+        guestCanvasRef.anchorMin = anchorPos;
     }
 }
