68a3cab8cf810989efeb8d65bb1345a25d54996b
FeliciaTorwald <113012104+FeliciaTorwald@users.noreply.github.com>
2023-05-12 10:43:28 +0200

    fixed bug in bobatheif guest now it doesnt need to be "served"

--- Cafe/Assets/Scripts/Julia/Boba_guests_follow_boba.cs
+++ Cafe/Assets/Scripts/Julia/Boba_guests_follow_boba.cs
@@ -147,8 +147,6 @@ public class Boba_guests_follow_boba : MonoBehaviour
             closestTeacup.transform.position = Boba_guests_pick_up_bobatea.transform.position;
             nollställ_huntBobatea = false;
 
-            closestTeacup.GetComponent<Interactable_NewFullTea>().StolenTea();
-            closestTeacup.GetComponent<Interactable_NewFullTea>().enabled = false;
             //Buggen med att teakoppen blir ett ufo runt spelaren när space klickas och man står när bobaätaren är troligen för att
             //När playern håller i bobateet och sen att boba ätaren tar den från playern så tror fortfarande playern att den håller 
             //i boba teet och därför när vi klickar på space så droppas teet men eftersom 
@@ -160,7 +158,6 @@ public class Boba_guests_follow_boba : MonoBehaviour
 
             //Get boba to stay in guests hand
             closestTeacup.GetComponent<Rigidbody>().isKinematic = true;
-
             if (StartDrinking_bobatea == false)
             {
                 guestDrinks_Timer = 0;
@@ -180,6 +177,7 @@ public class Boba_guests_follow_boba : MonoBehaviour
                 spawnOneDirtybobaTea = true;
             }
 
+
             if(spawnOneDirtybobaTea == true)
             {
             Destroy(closestTeacup);
@@ -189,6 +187,9 @@ public class Boba_guests_follow_boba : MonoBehaviour
             Boba_In_Hand = 0;
             spawnOneDirtybobaTea = false;
             }
+
+            closestTeacup.GetComponent<Interactable_NewFullTea>().StolenTea();
+            closestTeacup.GetComponent<Interactable_NewFullTea>().enabled = false;
         }
             //Walk to random position
             Random();
